1034674c854c376967d6742e3ae5edad0cfc6045
agyanim <agyanimconsultancy@gmail.com>
2023-07-27 14:36:00 +0000

    async functions added

--- Techathon-Node-class/week-5/note-Manage-Express/account.js
+++ Techathon-Node-class/week-5/note-Manage-Express/account.js
@@ -1,121 +1,119 @@
-const { readFile } = require("fs");
+const { readFile, readFileSync, writeFileSync } = require("fs");
 const cuid = require("cuid");
 const { writeToFile } = require("./handlingFiles");
 
-exports.createAccount = (req, res) => {
-	const { firstName, lastName, email, password } = req.body;
-	const account = {
-		userId: cuid(),
-		firstName: firstName,
-		lastName: lastName,
-		password: password,
-		email: email.toLowerCase(),
-		createdOn: new Date().toDateString().replace(/U/, " ").replace(/\..+/, ""),
-		modifiedOn: new Date().toDateString().replace(/U/, " ").replace(/\..+/, ""),
-	};
+exports.createAccount = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    const account = {
+      userId: cuid(),
+      firstName: firstName,
+      lastName: lastName,
+      password: password,
+      email: email.toLowerCase(),
+      createdOn: new Date()
+        .toDateString()
+        .replace(/U/, " ")
+        .replace(/\..+/, ""),
+      modifiedOn: new Date()
+        .toDateString()
+        .replace(/U/, " ")
+        .replace(/\..+/, ""),
+    };
 
-	if (!firstName || !lastName || !password || !email) {
-		return res.status(400).json({ message: "Sorry, all field are required" });
-	}
+    if (!firstName || !lastName || !password || !email) {
+      return res.status(400).json({ message: "Sorry, all field are required" });
+    }
 
-	readFile("./account.json", "utf-8", (err, data) => {
-		if (err) {
-			return res
-				.status(500)
-				.json({ message: "Sorry, internal server error occurred" });
-		}
-		data = JSON.parse(data);
-		data.push(account);
-		writeToFile("./account.json", JSON.stringify(data), res);
-		return res.status(201).json({ message: "success", account: data });
-	});
+    let data = await readFileSync("./account.json", "utf-8");
+    data = JSON.parse(data);
+    data.push(account);
+    writeFileSync("./account.json", JSON.stringify(data));
+    return res.status(201).json({ message: "success", account: data });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
-exports.getAllAccount = (req, res) => {
-	readFile("./account.json", "utf-8", (err, data) => {
-		if (err) {
-			return res
-				.status(500)
-				.json({ message: "Sorry, internal server error occurred" });
-		}
-		data = JSON.parse(data);
-		return res.status(200).json({ message: data });
-	});
+exports.getAllAccount = async (req, res) => {
+  try {
+    let data = await readFileSync("./account.json", "utf-8");
+    data = JSON.parse(data);
+    return res.status(200).json({ message: data });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
-exports.getAccountById = (req, res, id) => {
-	id = req.params.id;
-	if (!id) {
-		return res.status(400).json({ message: "User Id must be provided" });
-	}
-	readFile("./account.json", "utf-8", (err, filedata) => {
-		if (err) {
-			return res
-				.status(500)
-				.json({ message: "Sorry, internal server error occurred" });
-		}
-		filedata = JSON.parse(filedata);
-		const findUser = filedata.find((user) => user.userId == id);
-		if (!findUser) {
-			return res.status(200).json({ message: "no record found" });
-		}
-		return res.status(200).json({ message: "Success", account: findUser });
-	});
+exports.getAccountById = async (req, res, id) => {
+	console.log("get");
+  try {
+    id = req.params.id;
+    if (!id) {
+      return res.status(400).json({ message: "User Id must be provided" });
+    }
+    let filedata = readFileSync("./account.json", "utf-8");
+    filedata = JSON.parse(filedata);
+    const findUser = filedata.find((user) => user.userId == id);
+    if (!findUser) {
+      return res.status(200).json({ message: "no record found" });
+    }
+    return res.status(200).json({ message: "Success", account: findUser });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
-exports.updateAccount = (req, res, id) => {
-	id = req.params.id;
-	if (!id) {
-		return res.status(400).json({ message: "User Id must be provided" });
-	}
-	readFile("./account.json", "utf-8", (err, filedata) => {
-		if (err) {
-			return res
-				.status(500)
-				.json({ message: "Sorry, internal server error occurred" });
-		}
-		filedata = JSON.parse(filedata);
-		const findUser = filedata.find((user) => user.userId == id);
-		if (!findUser) {
-			return res.status(200).json({ message: "no record found" });
-		}
-		const filteredAccount = filedata.filter((account) => account.userId !== id);
+exports.updateAccount = async (req, res, id) => {
+  try {
+    const { firstName, lastName, email } = req.body;
+    id = req.params.id;
+    if (!id) {
+      return res.status(400).json({ message: "User Id must be provided" });
+    }
+    let filedata = await readFileSync("./account.json", "utf-8");
+    filedata = JSON.parse(filedata);
+    const findUser = filedata.find((user) => user.userId == id);
+    if (!findUser) {
+      return res.status(200).json({ message: "no record found" });
+    }
+    const filteredAccount = filedata.filter((account) => account.userId !== id);
 
-		req.body.firstName !== undefined
-			? (findUser.firstName = req.body.firstName)
-			: (findUser.firstName = findUser.firstName);
-		req.body.lastName !== undefined
-			? (findUser.lastName = req.body.lastName)
-			: (findUser.lastName = findUser.lastName);
-		req.body.email !== undefined
-			? (findUser.email = req.body.email)
-			: (findUser.email = findUser.email);
-		findUser.modifiedOn = new Date()
-			.toDateString()
-			.replace(/U/, " ")
-			.replace(/\..+/, "");
-		filteredAccount.push(findUser);
-		writeToFile("./account.json", JSON.stringify(filteredAccount), res);
-		return res.status(200).json({ message: "Success", account: findUser });
-	});
+    firstName !== undefined
+      ? (findUser.firstName = firstName)
+      : (findUser.firstName = findUser.firstName);
+    lastName !== undefined
+      ? (findUser.lastName = lastName)
+      : (findUser.lastName = findUser.lastName);
+    email !== undefined
+      ? (findUser.email = email)
+      : (findUser.email = findUser.email);
+    findUser.modifiedOn = new Date()
+      .toDateString()
+      .replace(/U/, " ")
+      .replace(/\..+/, "");
+    filteredAccount.push(findUser);
+   await writeFileSync("./account.json", JSON.stringify(filteredAccount));
+    return res.status(200).json({ message: "Success", account: findUser });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
-exports.deleteAccount = (req, res, id) => {
-	id = req.params.id;
-	if (!id) {
-		return res.status(400).json({ message: "User Id must be provided" });
-	}
-	readFile("./account.json", "utf-8", (err, filedata) => {
-		if (err) {
-			return res
-				.status(500)
-				.json({ message: "Sorry, internal server error occurred" });
-		}
-		filedata = JSON.parse(filedata);
-		const filteredAccount = filedata.filter((user) => user.userId !== id);
-		writeToFile("./account.json", JSON.stringify(filteredAccount), res);
-		return res
-			.status(200)
-			.json({ message: "Success", account: filteredAccount });
-	});
+exports.deleteAccount = async (req, res, id) => {
+  try {
+    id = req.params.id;
+    if (!id) {
+      return res.status(400).json({ message: "User Id must be provided" });
+    }
+    let filedata = await readFileSync("./account.json", "utf-8");
+    filedata = JSON.parse(filedata);
+    const filteredAccount = filedata.filter((user) => user.userId !== id);
+    await writeFileSync("./account.json", JSON.stringify(filteredAccount));
+    return res
+      .status(200)
+      .json({ message: "Success", account: filteredAccount });
+  } catch (error) {
+    return res.status(500).json({ error: error, message });
+  }
 };
--- Techathon-Node-class/week-5/note-Manage-Express/handlingFiles.js
+++ Techathon-Node-class/week-5/note-Manage-Express/handlingFiles.js
@@ -1,15 +1,3 @@
-const { writeFile } = require("fs").promises;
-
-exports.writeToFile= async (filePath,file,res)=>{
-try {
-    await  writeFile(filePath,file,"utf-8")
-} catch (error) {
-    res.writeHead(500, { "Content-Type": "application/json" });
-    return res.end(
-        JSON.stringify({ message: "Sorry, internal server error occurred" })
-    );
-}
-}
 
 exports.emailVarification=(email)=>{
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
